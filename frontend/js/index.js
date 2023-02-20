b4ec8428d8d34fc3cd5ef657e8ea42c1d8d00f8b
MehakKambo <mehakpreetp12@gmail.com>
2023-02-20 15:14:14 -0800

    Finished contact page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,3 +2,9 @@ function flipCard(id) {
     var card = document.getElementById(id);
     card.classList.toggle("clicked");
 }
+
+function messageReceived() {
+    let name = document.getElementById("name").value;
+    alert(name + ", thank you for reaching out! Your message has been received," + 
+            " we will get back to you within 24hrs.")
+}
